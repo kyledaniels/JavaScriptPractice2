ba20b2a34b375c2c17e14d91692694ed074b4cc9
kyle67 <daniels.kyle@gmail.com>
2020-02-27 18:32:46 -0500

    added sound to drum

--- Virtual Drum Practice/index.js
+++ Virtual Drum Practice/index.js
@@ -3,8 +3,13 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i=0; i<numberOfButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-          alert("I've been clicked");
+          ;
+
+          var audio = new Audio("./sounds/tom-1.mp3");
+           audio.play(".drum");
     });
 }
+
+
 
 
