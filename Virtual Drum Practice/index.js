b56aa9a4cfcc6f721582341ec06729b7c6314484
kyle67 <daniels.kyle@gmail.com>
2020-02-28 17:38:40 -0500

    Created control flow to play drum

--- Virtual Drum Practice/index.js
+++ Virtual Drum Practice/index.js
@@ -3,13 +3,33 @@ var numberOfButtons = document.querySelectorAll(".drum").length;
 
 for (var i=0; i<numberOfButtons; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function(){
-          ;
+          
+      switch (buttonInnerHTML) {
+          case "w":
+               var audio = new Audio("sounds/tom-1.mp3");
+               audio.play();
+               break;
 
-          var audio = new Audio("./sounds/tom-1.mp3");
-           audio.play(".drum");
+          case "a":
+              var audio = new Audio("sounds/tom-2.mp3");
+              audio.play();
+          case ""
+      
+          default:
+              break;
+      }
+
     });
 }
 
 
 
 
+
+
+
+
+
+
+// var audio = new Audio("./sounds/tom-1.mp3");
+// audio.play(".drum");
