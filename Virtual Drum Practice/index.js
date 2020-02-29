17a406765262afae90fc36a9452f17a2b01a13d9
kyle67 <daniels.kyle@gmail.com>
2020-02-29 08:01:39 -0500

    Added sound to drum

--- Virtual Drum Practice/index.js
+++ Virtual Drum Practice/index.js
@@ -6,14 +6,40 @@ for (var i=0; i<numberOfButtons; i++){
           
       switch (buttonInnerHTML) {
           case "w":
-               var audio = new Audio("sounds/tom-1.mp3");
-               audio.play();
+               var tom1 = new Audio("sounds/tom-1.mp3");
+               tom1.play();
                break;
 
           case "a":
-              var audio = new Audio("sounds/tom-2.mp3");
-              audio.play();
-          case ""
+              var tom2 = new Audio("sounds/tom-2.mp3");
+              tom2.play();
+              break;
+
+          case "s":
+              var tom3 = newAudio('sounds/tom-3.mp3');
+              tom3.play();
+              break;
+
+         case "d":
+             var tom4 = newAudio('sounds/tom-4.mp3');
+             tom4.play();
+             break;
+
+        case "j":
+            var snare = newAudio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "k":
+            var crash = newAudio('sounds/crash.mp3');
+            snare.play();
+            break;
+
+        case "l":
+        var kick = newAudio('kick/.mp3');
+        snare.play();
+        break;
+
       
           default:
               break;
