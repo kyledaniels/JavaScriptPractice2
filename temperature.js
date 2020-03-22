40d0fea56c5937e260234ba63c050fd36d853d4c
kyle67 <daniels.kyle@gmail.com>
2020-03-22 17:12:40 -0400

    delete incorrect file

--- temperature.js
+++
@@ -1,9 +0,0 @@
-// Set Kelvin equal too 293
-const kelvin = 0;
-const celsius = (kelvin - 273);
-//  Set variable for fahreheeit formula;
-var fahrenheit = celsius * (9/5) + 32;
-// Use math.floor to round decimal
-fahrenheit = Math.floor(celsius * (9/5) + 32)
-
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
