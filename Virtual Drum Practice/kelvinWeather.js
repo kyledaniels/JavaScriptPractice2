9815d49b4315a8da9279835bc8bf4d8e5885501d
kyle67 <daniels.kyle@gmail.com>
2020-07-02 01:46:00 -0400

    inserted formaula to calculate fahrenheit

--- Virtual Drum Practice/kelvinWeather.js
+++ Virtual Drum Practice/kelvinWeather.js
@@ -5,4 +5,6 @@ const kelvin = 293;
 const celsius = kelvin - 273;
 
 //calculate Fahrenheit
-let fahrenheit = celsius*((9/5)+32);
+let fahrenheit = Math.floor(celsius*((9/5)+32));
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
