91a9df69d5c0335938098fc89bd9d25a62d9c5c9
kyle67 <daniels.kyle@gmail.com>
2020-09-12 13:23:02 -0400

    added getter methods to check _energyLevel of robot

--- Objects/advanceObjects.js
+++ Objects/advanceObjects.js
@@ -1,4 +1,13 @@
 const robot = {
-    model:'1E78V2',
-    energyLevel:100,
+    _model: '1E78V2',
+    _energyLevel: 100,
+    get energyLevel() {
+      if(typeof this._energyLevel==='number'){
+         return `My current energy level is ${this._energyLevel}`
+      } else{
+        return 'System malfunction: cannot retrieve energy level'
+      }
+    }
   };
+  
+  console.log(robot.energyLevel);
