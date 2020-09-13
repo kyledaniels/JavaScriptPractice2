59e928f17790ee261ed6778adca23eb935506ce9
kyle67 <daniels.kyle@gmail.com>
2020-09-13 15:09:29 -0400

    used object.key, object.entries, and object.assign to modify robot

--- Objects/factoryFunctions.js
+++ Objects/factoryFunctions.js
@@ -11,3 +11,29 @@ const robotFactory = (model,mobile)=>{
 const tinCan = robotFactory('P-500', true );
 
 tinCan.beep();
+
+const robot = {
+	model: 'SAL-1000',
+  mobile: true,
+  sentient: false,
+  armor: 'Steel-plated',
+  energyLevel: 75
+};
+
+// What is missing in the following method call?
+const robotKeys = Object.keys(robot);
+
+console.log(robotKeys);
+
+// Declare robotEntries below this line:
+
+const robotEntries = Object.entries(robot);
+
+console.log(robotEntries);
+
+// Declare newRobot below this line:
+const newRobot= Object.assign({laserBlaster: true, voiceRecognition: true},robot)
+
+
+
+console.log(newRobot);
