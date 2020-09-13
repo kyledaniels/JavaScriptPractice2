da8e6363dd729e88f6ba9d21d8b16a06a714afb4
kyle67 <daniels.kyle@gmail.com>
2020-09-13 15:23:54 -0400

    created getters and setters for appetizers, mains,and desserts

--- Objects/mealMaker.js
+++ Objects/mealMaker.js
@@ -1,7 +1,32 @@
 const menu = {
     _courses:{
-      appetizers:[],
-      mains:[],
-      desserts:[]
+      _appetizers:[],
+      _mains:[],
+      _desserts:[]
+    },
+  
+    get appetizers(){
+  
+    },
+  
+    set appetizers(appetizerIn){
+      
+    },
+  
+    get mains(){
+  
+    },
+  
+    set mains(mainsIn){
+  
+    },
+  
+    get desserts(){
+  
+    },
+  
+    set desserts(desertsIn){
+  
     }
+  
   }
